85f2259a1088a3987aabe3a63abc04b49d2b6578
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-10 10:49:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
   # code here
   data[season].each do |people|
     binding.pry
-         people.each do |,v|
+         people.each do |k,v|
           if v == "Winner"
               full_name = people["name"]
               return full_name.split(' ').first
